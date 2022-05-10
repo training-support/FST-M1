1cea11b6ae075f20fd7ddfa0e65b4a261cb235d0
SumanjaliReddy <104206926+SumanjaliReddy@users.noreply.github.com>
2022-05-10 15:39:39 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity6.java
@@ -0,0 +1,40 @@
+package Activities;
+public class Activity6 {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+
+        //Open browser
+        driver.get("https://www.training-support.net/selenium/login-form");
+    }
+
+    @Test
+    @Parameters({"username", "password"})
+    public void loginTestCase(String username, String password) {
+        //Find username and pasword fields
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+
+        //Enter values
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+
+        //Click Log in
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        //Assert Message
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals(loginMessage, "Welcome Back, admin");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //Close browser
+        driver.close();
+    }
+
+}
