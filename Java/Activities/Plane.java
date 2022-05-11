cce20bbcce62cd5e8d5d32584f40fe5486642fc3
Shilpa1911 <104618161+Shilpa1911@users.noreply.github.com>
2022-05-11 15:27:23 +0530

    Add files via upload

---
+++ Java/Activities/Plane.java
@@ -0,0 +1,41 @@
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class Plane {
+
+	
+	
+	private List<String> passengers;
+    private int maxPassengers;
+    private Date lastTimeTookOf;
+    private Date lastTimeLanded;
+
+    public Plane(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+        this.passengers = new ArrayList<>();
+    }
+
+    public void onboard(String passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public Date takeOff() {
+        this.lastTimeTookOf = new Date();
+        return lastTimeTookOf;
+    }
+
+    public void land() {
+        this.lastTimeLanded = new Date();
+        this.passengers.clear();
+    }
+
+    public Date getLastTimeLanded() {
+        return lastTimeLanded;
+    }
+
+    public List<String> getPassengers() {
+        return passengers;
+    }
+}
