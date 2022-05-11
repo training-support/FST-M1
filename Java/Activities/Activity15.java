46091fa2537be2a1da80bf88728b3f2fd3c688a8
Shilpa1911 <104618161+Shilpa1911@users.noreply.github.com>
2022-05-11 15:25:33 +0530

    Add files via upload

---
+++ Java/Activities/Activity15.java
@@ -0,0 +1,52 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Activity15 {
+
+    private static final String FILE_NAME = "TestSheet.xlsx";
+
+    public static void main(String[] args) {
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Datatypes in Java");
+        Object[][] datatypes = {
+                {"Datatype", "Type", "Size(in bytes)"},
+                {"int", "Primitive", 2},
+                {"float", "Primitive", 4},
+                {"double", "Primitive", 8},
+                {"char", "Primitive", 1},
+                {"String", "Non-Primitive", "No fixed size"}
+        };
+
+        int rowNum = 0;
+        System.out.println("Creating excel");
+
+        for (Object[] datatype : datatypes) {
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (Object field : datatype) {
+                Cell cell = row.createCell(colNum++);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+    }
+}
---
+++ Java/Activities/Activity6.java
@@ -0,0 +1,23 @@
+
+public class Activity6 {
+
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+       
+        Plane plane = new Plane(10);
+      
+        plane.onboard("John");
+        plane.onboard("Steve");
+        plane.onboard("Anna");
+ 
+        System.out.println("Plane took off at: " + plane.takeOff());
+        //Print list of people on board
+        System.out.println("People on the plane: " + plane.getPassengers());
+        Thread.sleep(5000);
+         plane.land();
+        
+        System.out.println("Plane landed at: " + plane.getLastTimeLanded());
+        System.out.println("People on the plane after landing: " + plane.getPassengers());
+    }
+}
