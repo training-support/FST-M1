87e9706f0dc993b33a882629e8bb91761c5e2765
MeenaDhandapani <106802469+MeenaDhandapani@users.noreply.github.com>
2022-06-07 16:50:24 +0530

    Add files via upload

---
+++ Python/Activities/Activity23_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+# Create fixture
+@pytest.fixture
+def num_list():
+    list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+    # Alternatively
+    # list = list(range(11))
+    return list
+
+def test_sum(num_list):
+    sum = 0
+    for n in num_list:
+        sum += n
+    assert sum == 55
