1745eaa6278a2f5b1e5c9587296ce8ebc1661f1e
Sirisha1108 <nolli.sirisha@ibm.com>
2024-05-11 16:14:17 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,28 @@
+package Activities;
+
+public class Activity12 {
+
+    public interface Addable {
+        int add(int num1, int num2);
+    }
+
+    public static void main(String[] args) {
+
+        Addable ad1 = Integer::sum;
+
+        Addable ad2 = (num1, num2) -> {
+            int sum = num1 + num2;
+            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+            return sum;
+        };
+
+        // Use the lambda expressions
+        int result1 = ad1.add(10, 20);
+        System.out.println("Result from ad1: " + result1);
+
+        int result2 = ad2.add(30, 40);
+
+    }
+
+}
+
