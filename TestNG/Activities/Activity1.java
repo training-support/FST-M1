4384cf904e536ef889a36ae391a44f51addca82c
Jaanumouli <Kasaram.Moulika@ibm.com>
2024-06-11 20:34:32 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,53 @@
+package activites;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+    // Declare the WebDriver object
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeMethod() {
+        // Set up the Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+
+        //Open browser
+        driver.get("https://v1.training-support.net");
+    }
+
+    @Test
+    public void exampleTestCase() {
+        // Check the title of the page
+        String title = driver.getTitle();
+
+        //Print the title of the page
+        System.out.println("Page title is: " + title);
+
+        //Assertion for page title
+        Assert.assertEquals("Training Support", title);
+
+        //Find the clickable link on the page and click it
+        driver.findElement(By.id("about-link")).click();
+
+        //Print title of new page
+        System.out.println("New page title is: " + driver.getTitle());
+
+        Assert.assertEquals(driver.getTitle(), "About Training Support");
+    }
+
+    @AfterClass
+    public void afterMethod() {
+        //Close the browser
+        driver.quit();
+    }
+
+}
---
+++ TestNG/Activities/Activity42.java
@@ -0,0 +1,13 @@
+package activites;
+
+import org.testng.annotations.Test;
+
+public class Activity42
+{
+
+    @Test
+    public void TestCase()
+    {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
