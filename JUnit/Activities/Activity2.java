11c03ccce2f30e8b2731ee1d575a5cf995992c0e
Komal3315 <42232751+Komal3315@users.noreply.github.com>
2024-06-24 11:42:36 +0530

    Add files via upload

---
+++ JUnit/Activities/Activity2.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import activityPrograms.BankAccount;
+import activityPrograms.NotEnoughFundsException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Activity2 {
+
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10),
+                "Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
