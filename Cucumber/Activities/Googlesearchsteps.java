c391244c00f337d8ac6fbf0c747b7e6be6008773
Jaanumouli <Kasaram.Moulika@ibm.com>
2024-07-24 11:50:54 +0530

    Googlesearchsteps.java

---
+++ Cucumber/Activities/Googlesearchsteps.java
@@ -0,0 +1,49 @@
+package stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class GoogleSearchSteps
+{
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Given("^User is on Google Home Page$")
+
+    public void userIsOnGooglePage() throws Throwable {
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+        //wait = new WebDriverWait(driver, 15);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        //Open the browser
+        driver.get("https://www.google.com");
+    }
+
+    @When("^User types in Cheese and hits ENTER$")
+    public void userTypesInCheeseAndHitsENTER() throws Throwable {
+        driver.findElement(By.name("q")).sendKeys("Cheese", Keys.RETURN);
+    }
+
+    @Then("^Show how many search results were shown$")
+    public void showHowManySearchResultsWereShown() throws Throwable {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("result-stats")));
+        String resultStats = driver.findElement(By.id("result-stats")).getText();
+        System.out.println("Number of results found: " + resultStats);
+    }
+
+    @And("^Close the browser$")
+    public void closeTheBrowser() throws Throwable {
+        driver.close();
+    }
+}
