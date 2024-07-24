ca8d560e56939b8f5aa3109282b4a0d8a81ffc07
Jaanumouli <Kasaram.Moulika@ibm.com>
2024-07-24 11:06:15 +0530

    Activity5.java

---
+++ Appium/Project/Activity5.java
@@ -0,0 +1,62 @@
+package Appium_Project;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+
+public class Activity5
+{
+    AndroidDriver driver;
+    WebDriverWait wait;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.chrome");
+        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://localhost:4723/");
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        // Open the page in Chrome
+        driver.get("https://v1.training-support.net/selenium");
+    }
+    @Test
+    public void login() throws InterruptedException
+    {
+        String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.view.View")));
+        driver.findElement(AppiumBy.androidUIAutomator(UiScrollable + ".flingForward()"));
+
+        Thread.sleep(1000);
+        //driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"To-Do List\"]")).click();
+        driver.findElement(AppiumBy.xpath("")).click();
+
+        //Login Form  Please sign in.
+
+    }
+
+}
