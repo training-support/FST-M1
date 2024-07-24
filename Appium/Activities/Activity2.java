a43d24f12007af7fa066221306d8a2f95623d148
Jaanumouli <Kasaram.Moulika@ibm.com>
2024-07-24 10:58:22 +0530

    Activity2.java

---
+++ Appium/Activities/Activity2.java
@@ -0,0 +1,68 @@
+package Examples;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Activity2
+{
+    // Driver Declaration
+    AndroidDriver driver;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.chrome");
+        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://localhost:4723/");
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+
+        // Open the page in Chrome
+        driver.get("https://www.training-support.net");
+    }
+
+    // Test method
+    @Test
+    public void chromeTest() {
+        // Find heading on the page
+        String pageHeading = driver.findElement(AppiumBy.xpath(
+                "//android.view.View[@text='Training Support']"
+        )).getText();
+
+        // Print to console
+        System.out.println("Heading: " + pageHeading);
+
+        // Find and click the About Us link
+        driver.findElement(AppiumBy.xpath("//android.view.View[@resource-id='about-link']"
+        )).click();
+
+        // Find heading of new page and print to console
+        String aboutPageHeading = driver.findElement(AppiumBy.xpath(
+                "//android.view.View[@text='About Us']"
+        )).getText();
+        System.out.println(aboutPageHeading);
+    }
+
+
+    // Tear down method
+    @AfterClass
+    public void tearDown() {
+        // Close the app
+        driver.quit();
+    }
+}
