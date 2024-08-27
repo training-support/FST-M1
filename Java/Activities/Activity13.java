1f7d443b47cae80eb1d49bb9e37b71dc4cdc1415
Sirisha1108 <nolli.sirisha@ibm.com>
2024-08-27 14:10:23 +0530

    Add files via upload

---
+++ Java/Activities/Activity13.java
@@ -0,0 +1,26 @@
+package Activities;
+
+import java.util.*;
+
+public class Activity13 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Random indexGen = new Random();
+
+        System.out.print("Enter integers please ");
+        System.out.println("(EOF or non-integer to terminate): ");
+
+        while(scan.hasNextInt()) {
+            list.add(scan.nextInt());
+        }
+
+        Integer nums[] = list.toArray(new Integer[0]);
+        int index = indexGen.nextInt(nums.length);
+        System.out.println("Index value generated: " + index);
+        System.out.println("Value in array at generated index: " + nums[index]);
+
+        scan.close();
+    }
+}
