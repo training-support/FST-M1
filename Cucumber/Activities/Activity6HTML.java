a3678a8e621eae577d38e3e568f60125fe4c3511
Komal3315 <42232751+Komal3315@users.noreply.github.com>
2024-09-05 18:38:20 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity6HTML.java
@@ -0,0 +1,16 @@
+package testRunner;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"html: test-reports"},
+    monochrome = true
+)
+
+public class ActivitiesRunner {}
