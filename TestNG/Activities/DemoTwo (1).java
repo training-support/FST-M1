5ff364dfac2927ac46e853587f3a0d24e7b44a87
Komal3315 <42232751+Komal3315@users.noreply.github.com>
2024-09-06 14:36:59 +0530

    Delete TestNG/Activities/DemoTwo (1).java

--- TestNG/Activities/DemoTwo (1).java
+++
@@ -1,9 +0,0 @@
-package suiteExample;
-import org.testng.annotations.Test;
-
-public class DemoTwo {
-    @Test
-    public void TestCase() {
-        System.out.println("I'm in the test case from DemoTwo Class");
-    }
-}
